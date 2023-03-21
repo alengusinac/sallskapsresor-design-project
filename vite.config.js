5fb08ef75a5dd14c289f9f81f7cd358faa4b0ad1
Alen Gusinac <61206891+alengusinac@users.noreply.github.com>
2023-03-21 15:08:00 +0100

    fixed base URL

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 // Test1
 export default defineConfig({
-  base: '/Sallskapsresor-design-project/',
+  base: '/sallskapsresor-design-project/',
   define: {
     'process.env': {},
   },
